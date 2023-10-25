d75dca8ceda92bc77fe619432b262a27dcfdae56
Luis Fernando Sanchez <lf.sanchezgutierrez@ugto.mx>
2023-10-24 22:00:45 -0600

    First commit, upload the game

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_de_Gato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Juego de Gato con C#
